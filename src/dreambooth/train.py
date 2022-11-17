d6b83c22301366f86c8c084b06ae3c6a4fda31c7
Kirill Semin <k.semin@vk.team>
2022-11-17 18:31:41 +0300

    Implemented basic plain torch version

--- src/dreambooth/train.py
+++ src/dreambooth/train.py
@@ -1,12 +1,7 @@
-import argparse
 import functools
 import itertools
 import math
-import os
 from pathlib import Path
-from typing import Optional
-import subprocess
-import sys
 import bitsandbytes as bnb
 import pyrallis
 
@@ -15,13 +10,9 @@ import torch.nn.functional as F
 import torch.utils.checkpoint
 from torch.utils.data import Dataset
 
-from accelerate import Accelerator
 from accelerate.utils import set_seed
 from diffusers import AutoencoderKL, DDPMScheduler, StableDiffusionPipeline, UNet2DConditionModel
 from diffusers.optimization import get_scheduler
-from huggingface_hub import HfFolder, Repository, whoami
-from PIL import Image
-from torchvision import transforms
 from tqdm.auto import tqdm
 from transformers import CLIPTextModel, CLIPTokenizer
 from loguru import logger
@@ -31,173 +22,170 @@ from src.dreambooth.configs import TrainConfig
 from src.utils import convert2ckpt
 
 
-def train(
-    accelerator: Accelerator,
-    vae: AutoencoderKL,
-    unet: UNet2DConditionModel,
-    text_encoder: CLIPTextModel,
-    tokenizer: CLIPTokenizer,
-    train_dataset: DreamBoothDataset,
-    cfg: TrainConfig,
-    is_train_text_encoder: bool,
-):
-    if not is_train_text_encoder:
-        text_encoder.eval()
-        text_encoder.requires_grad_(False)
+class DreamBoothPipeline:
+    def __init__(self, cfg: TrainConfig):
+        self.cfg = cfg
 
-    if cfg.gradient_checkpointing:
-        unet.enable_gradient_checkpointing()
-        if is_train_text_encoder:
-            text_encoder.gradient_checkpointing_enable()
+        # Loading and freezing VAE
+        self.vae = AutoencoderKL.from_pretrained(cfg.model_path, subfolder="vae", use_auth_token=True)
+        self._freeze_model(self.vae)
+
+        self.text_encoder = CLIPTextModel.from_pretrained(cfg.model_path, subfolder="text_encoder", use_auth_token=True)
+        self.unet = UNet2DConditionModel.from_pretrained(cfg.model_path, subfolder="unet", use_auth_token=True)
+
+        self.tokenizer = CLIPTokenizer.from_pretrained(
+            cfg.model_path,
+            subfolder="tokenizer",
+        )
+
+        self.noise_scheduler = DDPMScheduler.from_config(cfg.model_path, subfolder="scheduler")
+        self.precision = torch.float32
+
+    def _freeze_model(self, model):
+        model.requires_grad_(False)
+        model.eval()
+
+    def _loss(self, noise, noise_pred, noise_prior=None, noise_prior_pred=None) -> torch.Tensor:
+        # Compute instance loss
+        loss = F.mse_loss(noise_pred.float(), noise.float(), reduction="none").mean([1, 2, 3]).mean()
+
+        # Compute prior loss
+        if noise_prior_pred and noise_prior:
+            prior_loss = F.mse_loss(noise_prior_pred.float(), noise_prior.float(), reduction="mean")
         else:
-            text_encoder.gradient_checkpointing_disable()
+            prior_loss = 0
 
-    # Use 8-bit Adam for lower memory usage or to fine-tune the model in 16GB GPUs
-    if cfg.use_8bit_adam:
-        optimizer_class = bnb.optim.AdamW8bit
-    else:
-        optimizer_class = torch.optim.AdamW
+        # Add the prior loss to the instance loss.
+        loss = loss + self.cfg.prior_loss_weight * prior_loss
 
-    params_to_optimize = (
-        itertools.chain(unet.parameters(), text_encoder.parameters()) if is_train_text_encoder else unet.parameters()
-    )
-    optimizer = optimizer_class(
-        params_to_optimize,
-        lr=cfg.lr,
-        betas=(cfg.adam_beta1, cfg.adam_beta2),
-        weight_decay=cfg.w_decay,
-        eps=cfg.adam_epsilon,
-    )
+        return loss
 
-    noise_scheduler = DDPMScheduler(
-        beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000
-    )
+    def _train_step(self, batch):
+        # Convert images to latent space
+        latents = self.vae.encode(batch["pixel_values"].to(dtype=self.precision)).latent_dist.sample()
+        latents = latents * 0.18215
 
-    collate_fn_ = functools.partial(collate_fn, with_prior_preservation=is_train_text_encoder, tokenizer=tokenizer)
-    train_dataloader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=cfg.batch_size, shuffle=True, collate_fn=collate_fn_
-    )
+        # Sample noise that we'll add to the latents
+        noise = torch.randn_like(latents)
+        bsz = latents.shape[0]
+        # Sample a random timestep for each image
+        timesteps = torch.randint(0, self.noise_scheduler.config.num_train_timesteps, (bsz,),
+                                  device=latents.device)
+        timesteps = timesteps.long()
 
-    total_batch_size = cfg.batch_size * accelerator.num_processes
-    n_steps_per_epoch = len(train_dataloader)
-    total_steps = cfg.n_steps_clip if is_train_text_encoder else cfg.n_steps
-    n_epochs = math.ceil(total_steps / n_steps_per_epoch)
+        # Add noise to the latents according to the noise magnitude at each timestep
+        # (this is the forward diffusion process)
+        noisy_latents = self.noise_scheduler.add_noise(latents, noise, timesteps)
 
-    lr_scheduler = get_scheduler(
-        cfg.lr_scheduler,
-        optimizer=optimizer,
-        num_warmup_steps=cfg.n_warmup_steps,
-        num_training_steps=total_steps,
-    )
+        # Get the text embedding for conditioning
+        encoder_hidden_states = self.text_encoder(batch["input_ids"])[0]
 
-    if is_train_text_encoder:
-        unet, text_encoder, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
-            unet, text_encoder, optimizer, train_dataloader, lr_scheduler
+        # Predict the noise residual
+        noise_pred = self.unet(noisy_latents, timesteps, encoder_hidden_states).sample
+
+        return noise, noise_pred
+
+    def train(self, n_steps: int, train_text_encoder: bool, train_unet: bool):
+        if self.cfg.gradient_checkpointing:
+            self.text_encoder.gradient_checkpointing_enable()
+            self.unet.gradient_checkpointing_enable()
+
+        if not train_text_encoder:
+            self.text_encoder.gradient_checkpointing_disable()
+            self._freeze_model(self.text_encoder)
+        if not train_unet:
+            self.unet.gradient_checkpointing_disable()
+            self._freeze_model(self.unet)
+
+        train_dataset = DreamBoothDataset(
+            instance_data_root=self.cfg.instance_data_folder,
+            instance_prompt=self.cfg.instance_prompt,
+            class_data_root=self.cfg.class_data_folder if train_text_encoder else None,
+            class_prompt=self.cfg.class_prompt,
+            tokenizer=self.tokenizer,
+            size=self.cfg.resolution,
+            center_crop=False,
         )
-    else:
-        unet, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
-            unet, optimizer, train_dataloader, lr_scheduler
+
+        params_to_optimize = (
+            itertools.chain(self.unet.parameters(),
+                            self.text_encoder.parameters()) if train_text_encoder else self.unet.parameters()
+        )
+        optimizer = torch.optim.AdamW(
+            params_to_optimize,
+            lr=self.cfg.lr,
+            betas=(self.cfg.adam_beta1, self.cfg.adam_beta2),
+            weight_decay=self.cfg.w_decay,
+            eps=self.cfg.adam_epsilon,
         )
 
-    weight_dtype = torch.float32
-    if cfg.precision == "fp16":
-        weight_dtype = torch.float16
-    elif cfg.precision == "bf16":
-        weight_dtype = torch.bfloat16
+        collate_fn_ = functools.partial(collate_fn, with_prior_preservation=train_text_encoder, tokenizer=self.tokenizer)
+        train_dataloader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=self.cfg.batch_size, shuffle=True, collate_fn=collate_fn_
+        )
 
-    # Move text_encode and vae to gpu.
-    # For mixed precision training we cast the text_encoder and vae weights to half-precision
-    # as these models are only used for inference, keeping weights in full precision is not required.
-    vae.to(accelerator.device, dtype=weight_dtype)
-    # if not cfg.train_text_encoder:
-    #     text_encoder.to(accelerator.device, dtype=weight_dtype)
+        n_steps_per_epoch = len(train_dataloader)
+        n_epochs = math.ceil(n_steps / n_steps_per_epoch)
 
-    # We need to initialize the trackers we use, and also store our configuration.
-    # The trackers initialize automatically on the main process.
-    if accelerator.is_main_process:
-        accelerator.init_trackers("dreambooth", config=vars(cfg))
+        lr_scheduler = get_scheduler(
+            self.cfg.lr_scheduler,
+            optimizer=optimizer,
+            num_warmup_steps=self.cfg.n_warmup_steps,
+            num_training_steps=n_steps,
+        )
 
-    # Train!
-    logger.info("***** Running training *****")
-    logger.info(f"  Num examples = {len(train_dataset)}")
-    logger.info(f"  Num batches each epoch = {len(train_dataloader)}")
-    logger.info(f"  Num Epochs = {n_epochs}")
-    logger.info(f"  Instantaneous batch size per device = {cfg.batch_size}")
-    logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {total_batch_size}")
-    logger.info(f"  Total optimization steps = {total_steps}")
+        # Train!
+        logger.info("***** Running training *****")
+        logger.info(f"  Num examples = {len(train_dataset)}")
+        logger.info(f"  Num batches each epoch = {len(train_dataloader)}")
+        logger.info(f"  Num Epochs = {n_epochs}")
+        logger.info(f"  Instantaneous batch size per device = {self.cfg.batch_size}")
+        logger.info(f"  Total train batch size (w. parallel, distributed & accumulation) = {self.cfg.batch_size}")
+        logger.info(f"  Total optimization steps = {n_steps}")
 
-    # Only show the progress bar once on each machine.
-    progress_bar = tqdm(range(total_steps), disable=not accelerator.is_local_main_process)
-    global_step = 0
+        # Only show the progress bar once on each machine.
+        progress_bar = tqdm(range(n_steps))
+        global_step = 0
 
-    for epoch in range(n_epochs):
-        unet.train()
-        if is_train_text_encoder:
-            text_encoder.train()
-        for step, batch in enumerate(train_dataloader):
-            with accelerator.accumulate(unet):
-                # Convert images to latent space
-                latents = vae.encode(batch["pixel_values"].to(dtype=weight_dtype)).latent_dist.sample()
-                latents = latents * 0.18215
+        for epoch in range(n_epochs):
+            if train_unet:
+                self.unet.train()
+            if train_text_encoder:
+                self.text_encoder.train()
 
-                # Sample noise that we'll add to the latents
-                noise = torch.randn_like(latents)
-                bsz = latents.shape[0]
-                # Sample a random timestep for each image
-                timesteps = torch.randint(0, noise_scheduler.config.num_train_timesteps, (bsz,), device=latents.device)
-                timesteps = timesteps.long()
+            for step, batch in enumerate(train_dataloader):
+                noise, noise_pred = self._train_step(batch)
+                noise_prior, noise_prior_pred = None, None
 
-                # Add noise to the latents according to the noise magnitude at each timestep
-                # (this is the forward diffusion process)
-                noisy_latents = noise_scheduler.add_noise(latents, noise, timesteps)
-
-                # Get the text embedding for conditioning
-                encoder_hidden_states = text_encoder(batch["input_ids"])[0]
-
-                # Predict the noise residual
-                noise_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
-
-                if is_train_text_encoder:
+                if train_text_encoder:
                     # Chunk the noise and noise_pred into two parts and compute the loss on each part separately.
-                    noise_pred, noise_pred_prior = torch.chunk(noise_pred, 2, dim=0)
+                    noise_pred, noise_prior_pred = torch.chunk(noise_pred, 2, dim=0)
                     noise, noise_prior = torch.chunk(noise, 2, dim=0)
 
-                    # Compute instance loss
-                    loss = F.mse_loss(noise_pred.float(), noise.float(), reduction="none").mean([1, 2, 3]).mean()
+                loss = self._loss(noise, noise_pred, noise_prior, noise_prior_pred)
 
-                    # Compute prior loss
-                    prior_loss = F.mse_loss(noise_pred_prior.float(), noise_prior.float(), reduction="mean")
-
-                    # Add the prior loss to the instance loss.
-                    loss = loss + cfg.prior_loss_weight * prior_loss
-                else:
-                    loss = F.mse_loss(noise_pred.float(), noise.float(), reduction="mean")
-
-                accelerator.backward(loss)
-                if accelerator.sync_gradients:
-                    params_to_clip = (
-                        itertools.chain(unet.parameters(), text_encoder.parameters())
-                        if is_train_text_encoder
-                        else unet.parameters()
-                    )
-                    accelerator.clip_grad_norm_(params_to_clip, cfg.max_grad_norm)
+                loss.backward()
                 optimizer.step()
                 lr_scheduler.step()
                 optimizer.zero_grad()
 
-            # Checks if the accelerator has performed an optimization step behind the scenes
-            if accelerator.sync_gradients:
                 progress_bar.update(1)
                 global_step += 1
                 progress_bar.set_postfix({"loss": loss.detach().item()})
 
-            logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
-            accelerator.log(logs, step=global_step)
+                logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
 
-            if global_step >= total_steps:
-                break
+                if global_step >= n_steps:
+                    break
 
-        accelerator.wait_for_everyone()
+    def save_sd(self):
+        pipeline = StableDiffusionPipeline.from_pretrained(
+            self.cfg.model_path,
+            unet=self.unet,
+            text_encoder=self.text_encoder,
+        )
+        pipeline.save_pretrained(self.cfg.output_dir)
+        convert2ckpt(self.cfg.output_dir)
 
 
 @pyrallis.wrap()
@@ -205,70 +193,12 @@ def main(cfg: TrainConfig):
     logging_dir = Path(cfg.output_dir, "logs/")
     set_seed(cfg.seed)
 
-    accelerator = Accelerator(
-        gradient_accumulation_steps=1,
-        mixed_precision="fp16",
-        logging_dir=logging_dir,
-    )
+    dreambooth_pipeline = DreamBoothPipeline(cfg)
 
-    text_encoder = CLIPTextModel.from_pretrained(cfg.model_path, subfolder="text_encoder", use_auth_token=True)
-    vae = AutoencoderKL.from_pretrained(cfg.model_path, subfolder="vae", use_auth_token=True)
-    unet = UNet2DConditionModel.from_pretrained(cfg.model_path, subfolder="unet", use_auth_token=True)
-    tokenizer = CLIPTokenizer.from_pretrained(
-        cfg.model_path,
-        subfolder="tokenizer",
-    )
+    dreambooth_pipeline.train(350, train_text_encoder=True, train_unet=True)
+    dreambooth_pipeline.train(1000, train_text_encoder=False, train_unet=True)
 
-    # Auto-encoder is always frozen
-    vae.requires_grad_(False)
-    vae.eval()
-
-    # First stage -- training clip text encoder + unet
-    train_dataset = DreamBoothDataset(
-        instance_data_root=cfg.instance_data_folder,
-        instance_prompt=cfg.instance_prompt,
-        class_data_root=cfg.class_data_folder,
-        class_prompt=cfg.class_prompt,
-        tokenizer=tokenizer,
-        size=cfg.resolution,
-        center_crop=False,
-    )
-    train(accelerator, vae, unet, text_encoder, tokenizer, train_dataset, cfg, is_train_text_encoder=True)
-
-    # Second stage -- training only unet
-    train_dataset = DreamBoothDataset(
-        instance_data_root=cfg.instance_data_folder,
-        instance_prompt=cfg.instance_prompt,
-        class_data_root=None,
-        tokenizer=tokenizer,
-        size=cfg.resolution,
-        center_crop=False,
-    )
-
-    # Freeze text encoder
-    text_encoder.requires_grad_(False)
-    text_encoder.eval()
-
-    # Reset unet weights
-    # unet = UNet2DConditionModel.from_pretrained(cfg.model_path, subfolder="unet", use_auth_token=True)
-
-    # Start training
-    train(accelerator, vae, unet, text_encoder, tokenizer, train_dataset, cfg, is_train_text_encoder=False)
-
-    # Create the pipeline using the trained modules and save it.
-    if accelerator.is_main_process:
-        unet = accelerator.unwrap_model(unet)
-        text_encoder = accelerator.unwrap_model(text_encoder)
-
-        pipeline = StableDiffusionPipeline.from_pretrained(
-            cfg.model_path,
-            unet=unet,
-            text_encoder=text_encoder,
-        )
-        pipeline.save_pretrained(cfg.output_dir)
-        convert2ckpt(cfg.output_dir)
-
-    accelerator.end_training()
+    dreambooth_pipeline.save_sd()
 
 
 if __name__ == "__main__":
